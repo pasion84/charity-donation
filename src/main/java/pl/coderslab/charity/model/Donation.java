20dc3da28092583d65d31b26c890b162b5febec0
Grzegorz Kalfas <pasion84@gmail.com>
2019-10-21 19:18:35 +0200

    encja Donation

---
+++ src/main/java/pl/coderslab/charity/model/Donation.java
@@ -0,0 +1,101 @@
+package pl.coderslab.charity.model;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "donation")
+public class Donation extends AbstractEntity {
+    private Integer quantity;
+    @OneToMany
+    private List<Category> categories = new ArrayList<>();
+    @OneToOne
+    private Institution institution;
+    private String street;
+    private String city;
+    private String zipCode;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private String pickUpComment;
+
+    public Donation() {
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public Institution getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(Institution institution) {
+        this.institution = institution;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public LocalDate getPickUpDate() {
+        return pickUpDate;
+    }
+
+    public void setPickUpDate(LocalDate pickUpDate) {
+        this.pickUpDate = pickUpDate;
+    }
+
+    public LocalTime getPickUpTime() {
+        return pickUpTime;
+    }
+
+    public void setPickUpTime(LocalTime pickUpTime) {
+        this.pickUpTime = pickUpTime;
+    }
+
+    public String getPickUpComment() {
+        return pickUpComment;
+    }
+
+    public void setPickUpComment(String pickUpComment) {
+        this.pickUpComment = pickUpComment;
+    }
+}
--- src/main/java/pl/coderslab/charity/services/InstitutionService.java
+++ src/main/java/pl/coderslab/charity/services/InstitutionService.java
@@ -6,9 +6,4 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class InstitutionService {
-    private InstitutionService institutionService;
-
-    public InstitutionService(InstitutionService institutionService) {
-        this.institutionService = institutionService;
-    }
 }
